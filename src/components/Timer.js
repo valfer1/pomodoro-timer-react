4e2b4b77cbb4e2c101f8f7d501cb2265cbc4df94
Valeria <valzilla@ymail.com>
2019-03-19 00:18:39 -0300

    .

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -23,13 +23,56 @@ export default class Timer extends Component {
         this.setState({
             time: this.times.defaultTime
         })
-    }
+		}
+		
+		displayTimer = seconds => {
+			const min = Math.floor(seconds % 3600 / 60);
+			const sec = Math.floor(seconds % 3600 % 60);
+
+			return `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`
+		}
+
+		setTime = newTime => {
+
+		}
+
+		restartInterval = () => {
+
+		}
+
+		countDown = () => {
+			
+		}
+
+		setTimeForWork = () => {
+
+		}
+
+		setTimeForShortBreak = () => {
+
+		}
+
+		setTimeForLongBreak = () => {
+
+		}
+
   render() {
       const { alert: { type, message }, time} = this.state;
-      
+
     return (
       <div>
-        
+        <h1>POMODORO TIMER</h1>
+				<div className={`alert ${type}`}>
+					{message}
+				</div>
+        <div className="time-container">
+					{this.displayTimer(time)}
+        </div>
+				<div className="btns">
+					<button className="start" onClick={this.setTimeForWork}>Start working</button>
+					<button className="short" onClick={this.setTimeForShortBreak}>Short break</button>
+					<button className="long" onClick={this.setTimeForLongBreak}>Long break</button>
+				</div>
       </div>
     )
   }
